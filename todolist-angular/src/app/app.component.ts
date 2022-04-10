e36cab5197ef3d7d8bfa86a175c467d42c54869b
jason cheng <jason.cheng@lileesystems.com>
2022-04-10 13:08:18 +0800

    add angular todo list template

--- todolist-angular/src/app/app.component.ts
+++ todolist-angular/src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todolist-angular';
+  // todoStore: TodoStore;
+	newTodoText = '';
+
+	// constructor(todoStore: TodoStore) {
+	// 	this.todoStore = todoStore;
+	// }
+
+	// stopEditing(todo: Todo, editedTitle: string) {
+	// 	todo.title = editedTitle;
+	// 	todo.editing = false;
+	// }
+
+	// cancelEditingTodo(todo: Todo) {
+	// 	todo.editing = false;
+	// }
+
+	// updateEditingTodo(todo: Todo, editedTitle: string) {
+	// 	editedTitle = editedTitle.trim();
+	// 	todo.editing = false;
+
+	// 	if (editedTitle.length === 0) {
+	// 		return this.todoStore.remove(todo);
+	// 	}
+
+	// 	todo.title = editedTitle;
+	// }
+
+	// editTodo(todo: Todo) {
+	// 	todo.editing = true;
+	// }
+
+	// removeCompleted() {
+	// 	this.todoStore.removeCompleted();
+	// }
+
+	// toggleCompletion(todo: Todo) {
+	// 	this.todoStore.toggleCompletion(todo);
+	// }
+
+	// remove(todo: Todo){
+	// 	this.todoStore.remove(todo);
+	// }
+
+	// addTodo() {
+	// 	if (this.newTodoText.trim().length) {
+	// 		this.todoStore.add(this.newTodoText);
+	// 		this.newTodoText = '';
+	// 	}
+	// }
 }
