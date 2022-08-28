2ecab17b3b6dae95f2b3002f1e4ccf96c60afd48
jason zheng <tso1158687@gmail.com>
2022-08-28 13:34:26 +0800

    [react] add todo content

--- todolist-react/src/App.tsx
+++ todolist-react/src/App.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Header from './components/header';
-import Content from './components/content';
-import Footer from './components/footer';
-import { Todo } from './type/todo.type';
+import "./App.css";
+import Header from "./components/header";
+import Content from "./components/content";
+import Footer from "./components/footer";
+import { Todo } from "./type/todo.type";
 function App() {
   const defaultTodoList: Todo[] = [
     {
-      content: '吃飯',
+      content: "吃飯",
       id: 1,
       completed: false,
     },
     {
-      content: '睡覺',
+      content: "睡覺",
       id: 2,
       completed: false,
     },
   ];
   return (
     <div className="App">
-      <Header/>
-      <Content/>
-      <Footer/>
+      <Header />
+      <Content todoList={defaultTodoList} />
+      <Footer />
     </div>
   );
 }
--- todolist-react/src/components/content.tsx
+++ todolist-react/src/components/content.tsx
@@ -1,15 +1,24 @@
-const Content = () => {
+import { Todo } from "../type/todo.type";
+import { useState } from "react";
+
+const Content = (prop: { todoList: Todo[] }) => {
+  const { todoList } = prop;
+  console.log(todoList);
+  const [todoListInside, setTodoListInside] = useState<Todo[]>(todoList);
+  console.log(todoListInside);
   return (
     <section className="main">
       <input className="toggle-all" type="checkbox" />
       <ul className="todo-list">
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>content</label>
-            <button className="destroy"></button>
-          </div>
-        </li>
+        {todoListInside.map((todo: Todo) => (
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>{todo.content}</label>
+              <button className="destroy"></button>
+            </div>
+          </li>
+        ))}
       </ul>
     </section>
   );
