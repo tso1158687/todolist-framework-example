8e54bb80f1173b78b8840fe1028d0398ea4a8576
jason zheng <tso1158687@gmail.com>
2024-11-22 11:27:33 +0800

    [angular] update to 19

--- todolist-angular/src/app/app.component.ts
+++ todolist-angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { TodoService } from './todo.service';
 import { Todo } from './type/todo.type';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent implements OnInit {
   todoList: Todo[] = [];
