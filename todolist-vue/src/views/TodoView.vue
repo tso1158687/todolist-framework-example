94d4fe11f29dee14950fc71779d65ffe7c51b7ca
jason zheng <tso1158687@gmail.com>
2025-01-24 12:56:01 +0800

    [vue] Adjust vue to todo list template

---
+++ todolist-vue/src/views/TodoView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const newTodo = ref('')
+const addTodo = () => {
+    if (newTodo.value.trim() === '') {
+        return
+    }
+   console.log(newTodo.value)
+    newTodo.value = ''
+}
+</script>
+
+<template>
+    <header class="header">
+
+        <input class="new-todo" placeholder="What needs to be done?" autofocus @keyup.enter="addTodo"
+            :model="newTodo" />
+    </header>
+
+
+</template>
